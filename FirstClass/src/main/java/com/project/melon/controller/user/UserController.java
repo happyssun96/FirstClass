264060127f2b3a2399848d50a48513a07d8f93c0
leedg <leedg@DG-Gram>
2021-12-09 17:09:35 +0900

    UserController userDetailInformation 작업 수행 MemberVo 파일 수정

--- FirstClass/src/main/java/com/project/melon/controller/user/UserController.java
+++ FirstClass/src/main/java/com/project/melon/controller/user/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.project.melon.model.MemberVo;
 import com.project.melon.service.MemberService;
 
 @Controller
@@ -21,9 +23,25 @@ public class UserController {
 		private MemberService memberService;
 	
 		@RequestMapping(value = "searchPage.do", method = RequestMethod.GET)
-			public String userDetailInformation(HttpSession session, Model model)
-			{
-			return "main/user/UserDetailInformationForm";
-			}
-
-}
+		public String searchList(@RequestParam(defaultValue = "1")int curPage, 
+				@RequestParam(defaultValue="")String keyword, Model model)
+		{
+			
+			return "";
+		}
+		
+		@RequestMapping(value = "userDetailInformation.do")
+		public String userDetailInformation(HttpSession session, String password, Model model)
+		{
+		// 원래는 사용자 비밀번호를 입력받고 해야하지만 우선은 패스
+		
+		MemberVo tempVo = (MemberVo) session.getAttribute("MemberVo");
+		if(tempVo.getPassword().equals(password))
+		{
+			model.addAttribute(tempVo);
+			return "main/user/UserDetailInformationForm";	
+		}
+		
+		return "main/user/UserValidFail"; // 비밀번호가 일치하지 않는경우 비밀번호 검증 실패 페이지로 이동
+		}
+	}
